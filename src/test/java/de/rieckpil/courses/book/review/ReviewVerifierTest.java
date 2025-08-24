539eb3c5ca6e078de378443e492483a528aef2c8
irah rosete <72952642+irahrosete@users.noreply.github.com>
2025-08-24 13:29:17 +1000

    clean up test and add message when assertion fails

--- src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
@@ -2,48 +2,23 @@ package de.rieckpil.courses.book.review;
 
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 class ReviewVerifierTest {
 
   private ReviewVerifier reviewVerifier;
 
-  public ReviewVerifierTest() {
-    System.out.println("Constructor called");
-  }
-
   @BeforeEach
   void setup() {
-    System.out.println("Before each");
     reviewVerifier = new ReviewVerifier();
-  }
-
-  @AfterEach
-  void tearDown() {
-    System.out.println("After each");
-  }
-
-  @BeforeAll
-  static void beforeAll() {
-    System.out.println("Before all");
-  }
-
-  @AfterAll
-  static void afterAll() {
-    System.out.println("After all");
   }
 
   @Test
   void shouldFailWhenReviewContainsSwearWords() {
     String review = "This book is shit.";
     System.out.println("Testing a review");
-    boolean result = reviewVerifier.doesMeetQualityStandards(review);
-    Assertions.assertFalse(result);
-  }
 
-  @Test
-  void shouldFailWhenReviewContainsSwearWords2() {
-    String review = "This book is shit.";
-    System.out.println("Testing a review2");
     boolean result = reviewVerifier.doesMeetQualityStandards(review);
-    Assertions.assertFalse(result);
+    assertFalse(result, "ReviewVerifier detected a swear word.");
   }
 }
