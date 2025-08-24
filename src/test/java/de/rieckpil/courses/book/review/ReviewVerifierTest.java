9848612da2e89edbe72dd0334681ca8cc9f6143e
irah rosete <72952642+irahrosete@users.noreply.github.com>
2025-08-24 13:16:39 +1000

    use lifecycle of a test

--- src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
@@ -1,15 +1,48 @@
 package de.rieckpil.courses.book.review;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class ReviewVerifierTest {
+
+  private ReviewVerifier reviewVerifier;
+
+  public ReviewVerifierTest() {
+    System.out.println("Constructor called");
+  }
+
+  @BeforeEach
+  void setup() {
+    System.out.println("Before each");
+    reviewVerifier = new ReviewVerifier();
+  }
+
+  @AfterEach
+  void tearDown() {
+    System.out.println("After each");
+  }
+
+  @BeforeAll
+  static void beforeAll() {
+    System.out.println("Before all");
+  }
+
+  @AfterAll
+  static void afterAll() {
+    System.out.println("After all");
+  }
 
   @Test
   void shouldFailWhenReviewContainsSwearWords() {
     String review = "This book is shit.";
+    System.out.println("Testing a review");
+    boolean result = reviewVerifier.doesMeetQualityStandards(review);
+    Assertions.assertFalse(result);
+  }
 
-    ReviewVerifier reviewVerifier = new ReviewVerifier();
+  @Test
+  void shouldFailWhenReviewContainsSwearWords2() {
+    String review = "This book is shit.";
+    System.out.println("Testing a review2");
     boolean result = reviewVerifier.doesMeetQualityStandards(review);
     Assertions.assertFalse(result);
   }
