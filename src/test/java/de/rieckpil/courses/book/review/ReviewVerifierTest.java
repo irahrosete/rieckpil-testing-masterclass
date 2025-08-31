a5ac39dcbfbc6b6ace6284b7835243f568673af1
irah rosete <72952642+irahrosete@users.noreply.github.com>
2025-09-01 08:38:37 +1000

    apply spotless check

--- src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
@@ -1,5 +1,7 @@
 package de.rieckpil.courses.book.review;
 
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
@@ -10,8 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-
-import java.util.List;
 
 import static de.rieckpil.courses.book.review.RandomReviewParameterResolverExtension.RandomReview;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -84,30 +84,29 @@ class ReviewVerifierTest {
   @Test
   void shouldPassWhenReviewIsGoodHamcrest() {
     String review =
-      "I can totally recommend this book to anyone interested in learning how to write Java code!";
+        "I can totally recommend this book to anyone interested in learning how to write Java code!";
     boolean result = reviewVerifier.doesMeetQualityStandards(review);
-//    assertTrue(result, "ReviewVerifier did not detect a good review"); Junit 5
+    //    assertTrue(result, "ReviewVerifier did not detect a good review"); Junit 5
 
-    MatcherAssert.assertThat("ReviewVerifier did not detect a good review", result, Matchers.equalTo(true));
+    MatcherAssert.assertThat(
+        "ReviewVerifier did not detect a good review", result, Matchers.equalTo(true));
     MatcherAssert.assertThat("Lorem ipsum", Matchers.endsWith("ipsum"));
-    MatcherAssert.assertThat(List.of(1,2,3,4,5), Matchers.hasSize(5));
-    MatcherAssert.assertThat(List.of(1,2,3,4,5), Matchers.anyOf(Matchers.hasSize(5), Matchers.emptyIterable()));
+    MatcherAssert.assertThat(List.of(1, 2, 3, 4, 5), Matchers.hasSize(5));
+    MatcherAssert.assertThat(
+        List.of(1, 2, 3, 4, 5), Matchers.anyOf(Matchers.hasSize(5), Matchers.emptyIterable()));
   }
 
   @Test
   void shouldPassWhenReviewIsGoodAssertJ() {
     String review =
-      "I can totally recommend this book to anyone interested in learning how to write Java code!";
+        "I can totally recommend this book to anyone interested in learning how to write Java code!";
     boolean result = reviewVerifier.doesMeetQualityStandards(review);
-//    assertTrue(result, "ReviewVerifier did not detect a good review"); JUnit 5
+    //    assertTrue(result, "ReviewVerifier did not detect a good review"); JUnit 5
 
     Assertions.assertThat(result)
-      .withFailMessage("ReviewVerifier did not detect a good review")
-      .isTrue();
-    Assertions.assertThat(List.of(1,2,3,4,5))
-      .hasSizeBetween(1,10);
-    Assertions.assertThat(List.of(1,2,3,4,5))
-      .contains(3)
-      .isNotEmpty();
+        .withFailMessage("ReviewVerifier did not detect a good review")
+        .isTrue();
+    Assertions.assertThat(List.of(1, 2, 3, 4, 5)).hasSizeBetween(1, 10);
+    Assertions.assertThat(List.of(1, 2, 3, 4, 5)).contains(3).isNotEmpty();
   }
 }
