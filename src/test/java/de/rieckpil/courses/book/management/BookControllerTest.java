b240fe695ab2c715734adecf2d6efd76ffe6db47
irah rosete <72952642+irahrosete@users.noreply.github.com>
2025-09-02 17:38:27 +1000

    create test for book controller getAvailableBooks using MockMvc

--- src/test/java/de/rieckpil/courses/book/management/BookControllerTest.java
+++ src/test/java/de/rieckpil/courses/book/management/BookControllerTest.java
@@ -1,18 +1,41 @@
 package de.rieckpil.courses.book.management;
 
+import de.rieckpil.courses.config.WebSecurityConfig;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.apache.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BookController.class)
+@Import(WebSecurityConfig.class)
 class BookControllerTest {
 
   @MockitoBean
   private BookManagementService bookManagementService;
 
-  @Test
-  void shouldStart() {
+  @Autowired
+  private MockMvc mockMvc;
 
+  @Test
+  void shouldGetEmptyArrayWhenNoBooksExist() throws Exception {
+    MvcResult mvcResult = mockMvc
+      .perform(get("/api/books")
+        .header(ACCEPT, APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(content().contentType(APPLICATION_JSON))
+      .andExpect(jsonPath("$.size()", Matchers.is(0)))
+      .andDo(print())
+      .andReturn();
   }
 
 }
