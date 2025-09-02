af081921a313b409518ccf56de6cfda2abdb2a9c
irah rosete <72952642+irahrosete@users.noreply.github.com>
2025-09-02 14:12:26 +1000

    use test container for postgres to test review repository

--- src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryInMemoryTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryInMemoryTest.java
@@ -7,11 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
@@ -20,23 +15,16 @@ import static java.time.LocalDateTime.now;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@DataJpaTest
-@Testcontainers
+@DataJpaTest(
+    properties = {
+      "spring.flyway.enabled=false", // to disable flyway and let h2 in-memory kick in. not
+      // advisable in production
+      "spring.jpa.hibernate.ddl-auto=create-drop",
+      "spring.datasource.driver-class-name=com.p6spy.engine.spy.P6SpyDriver", // P6Spy
+      "spring.datasource.url=jdbc:p6spy:h2:mem:testing;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=false" // P6Spy
+    })
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class ReviewRepositoryTest {
-
-  @Container
-  static PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:12")
-    .withDatabaseName("test")
-    .withUsername("duke")
-    .withPassword("s3cret");
-
-  @DynamicPropertySource
-  static void properties(DynamicPropertyRegistry registry) {
-    registry.add("spring.datasource.url", container::getJdbcUrl);
-    registry.add("spring.datasource.username", container::getUsername);
-    registry.add("spring.datasource.password", container::getPassword);
-  }
+class ReviewRepositoryInMemoryTest {
 
   @Autowired private EntityManager entityManager;
 
@@ -59,7 +47,27 @@ class ReviewRepositoryTest {
     assertNotNull(dataSource);
 
     System.out.println(dataSource.getConnection().getMetaData().getDatabaseProductName());
+  }
 
+  @Test
+  void testSave() {
+    Review review = new Review();
+    review.setTitle("Review 101");
+    review.setContent("Great book!");
+    review.setCreatedAt(now());
+    review.setRating(5);
+    review.setBook(null);
+    review.setUser(null);
+
+    //    Review result = cut.save(review);
+    Review result = testEntityManager.persistAndFlush(review);
+
+    System.out.println(result);
+    assertNotNull(result.getId());
+  }
+
+  @Test
+  void transactionSupportTest() {
     Review review = new Review();
     review.setTitle("Review 101");
     review.setContent("Great book!");
